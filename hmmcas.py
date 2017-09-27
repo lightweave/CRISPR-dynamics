246db53d2310293d56e773b1dff0e90e5ea999b1
lightweave <brilkov@gmail.com>
2017-09-27 11:48:24 +0300

    2 stage

--- hmmcas.py
+++ hmmcas.py
@@ -1,4 +1,7 @@
-#
+# before use run in command shell:
+# pip install pipenv
+# pipenv install requests
+
 import requests
 import re
 
@@ -8,20 +11,25 @@ r = requests.get('http://immunet.cn/hmmcas') # this is optional
 url = "http://immunet.cn/hmmcas/upload2.php"
 payload = {'sequence': '>gi|347755963|ref|YP_004863525.1| hypothetical protein [Candidatus Chloracidobacterium thermophilum B] MSDTTPLPFPPHYDPIQTVAAEYPGTAPQALFAPAMAWQQAHSLRPAADDARRAHLLVIDMQVDFCFPSGTLYVAGRSGTGGTDALRRTVEFMYRYLPWISEITCTLDSHVPGQVFFPGAHLTEDGAPVAPHTVITAADYRAGRYRPNPALAAQLGVTTEWLTHQVTDYCTRLEATGKYALYVWPYHCLVGTAGHRLAGVLADACLFHAFARGAANAPVLKGDSPLTENYSVFAPEVTTCWDGQPMPGAVRHEALLKRLLTADVILVAGLASSHCVAASVADLLAFVQEHNPYLAHRIVLLRDAMAPVVVPGADFTDAAEQALASFEAAGARVLTTDDPVEAWWG', 
 			'key2': 'value2'}
+
+print("request to "+ url)		
 r = requests.post(url, data=payload)
 
 
-print(r.text)
+#print(r.text)
 
 
-
+# button for:
+#url2 = "http://i.uestc.edu.cn/hmmcas/operon.php?qsf=ToSlide175938&tb=Table175938&sp=3"
+#url2 = "http://i.uestc.edu.cn/hmmcas/operon.php?qsf=ToSlide031405&tb=Table031405&sp=3"
+# Search links on buttons:
 matches = re.findall("(http\:\/\/i\.uestc\.edu\.cn\/hmmcas\/operon\.php\?qsf=ToSlide.\d+\&tb=Table\d+\&sp=3)", r.text)
 
-print(matches)
-# button for:
-url2 = "http://i.uestc.edu.cn/hmmcas/operon.php?qsf=ToSlide175938&tb=Table175938&sp=3"
-url2 = "http://i.uestc.edu.cn/hmmcas/operon.php?qsf=ToSlide031405&tb=Table031405&sp=3"
+print("found " + matches[1])
 
+url2 = matches[1]
+
+r = requests.get(url2)
 
 
 
